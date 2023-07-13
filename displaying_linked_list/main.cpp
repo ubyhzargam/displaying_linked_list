71a506caf14b65e73576af16091cf86e2586a9ce
ubyhzargam <ubyhzargam@gmail.com>
2023-07-13 17:50:11 +0530

    Update main.cpp

--- displaying_linked_list/main.cpp
+++ displaying_linked_list/main.cpp
@@ -6,9 +6,51 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *next;
+}*first=NULL;
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t,*last;
+    first = (struct Node*)malloc(sizeof(struct Node));
+    first->data=A[0];
+    first->next=NULL;
+    last=first;
+    for(i=1;i<n;i++)
+    {
+        t=(struct Node*)malloc(sizeof(struct Node));
+        t->data=A[i];
+        t->next=NULL;
+        last->next=t;
+        last=t;
+    }
+}
+void Display1(struct Node *p)
+{
+    if(p!=NULL)
+    {
+        cout<<p->data<<" ";
+        Display1(p->next);
+    }
+}
+void Display2(struct Node *p)
+{
+    if(p!=NULL)
+    {
+        Display2(p->next);
+        cout<<p->data<<" ";
+    }
+}
+int main()
+{
+    int A[]={1,2,3,4,5};
+    create(A,5);
+    Display1(first);
+    cout<<endl;
+    Display2(first);
     return 0;
 }
